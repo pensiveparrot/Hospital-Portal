90a327e9b70421d0a17cc16fc69a38b6848606f4
Dillon F <54560136+pensiveparrot@users.noreply.github.com>
2022-01-24 11:42:48 -0500

    push spring

---
+++ hospital-demo/src/test/java/com/hospitalportal/base/HospitalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalportal.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
