d0568a1aa619f44952ed73ec13205d3000d1889a
Dillon F <54560136+pensiveparrot@users.noreply.github.com>
2022-01-26 12:50:42 -0500

    admin and pom
    login service test

--- hospital-demo/src/main/java/com/hospitalportal/base/repository/AdminRepository.java
+++ hospital-demo/src/main/java/com/hospitalportal/base/repository/AdminRepository.java
@@ -1,9 +1,13 @@
 package com.hospitalportal.base.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hospitalportal.base.auth.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
 		Admin findById(String Id);
+		
+		Optional<Admin> findByLoginAndPassword(String Id, String password);
 }
--- hospital-demo/src/main/java/com/hospitalportal/base/service/AdminService.java
+++ hospital-demo/src/main/java/com/hospitalportal/base/service/AdminService.java
@@ -1,14 +1,22 @@
 package com.hospitalportal.base.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hospitalportal.base.auth.Admin;
 import com.hospitalportal.base.repository.AdminRepository;
 
 @Service
 public class AdminService {
 
-		private final AdminRepository adminRepository;
+@Autowired
+private AdminRepository adminRepository;
 		public AdminService(AdminRepository adminRepository) {
 			this.adminRepository=adminRepository;
+		}
+		public Admin login(String id, String password)
+		{
+				return(adminRepository.findByLoginAndPassword(id, password).orElse(null));
+			
 		}
 }
