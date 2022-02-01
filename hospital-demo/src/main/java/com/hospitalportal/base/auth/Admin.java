f6c3028697600c06bc5f67c5c3d16cdd73ae9266
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2022-02-01 10:57:13 -0600

    Add files via upload

---
+++ hospital-demo/src/main/java/com/hospitalportal/base/auth/Admin.java
@@ -0,0 +1,34 @@
+package com.hospitalportal.base.auth;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+@Entity
+@Table(name="admin_login")
+public class Admin {
+	public Employee getId() {
+		return id;
+	}
+	public void setId(Employee id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ManyToOne
+	@JoinColumn(name="employee_id",nullable=false)
+	private Employee id;
+	@Column(name="password")
+	private String password;
+	
+}
